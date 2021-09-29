67c03f325be051d519dc910d0c6ce0d8e354fd71
MeyerKP <mey.kevin@gmail.com>
2021-09-28 23:24:38 -0500

    Add main menu div and JS open functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,10 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+document.getElementById("navmenu").onclick = openmainMenu
+  function openmainMenu() {
+    document.getElementById("main_menu").style.right = '0px';
+    document.getElementById("main_menu").style.height = 'auto';
+    document.getElementById("main_menu").style.width = '100%';
+  }
