a2bf85ca9f1814572583c95b895e9a024445886c
MeyerKP <mey.kevin@gmail.com>
2021-10-12 23:08:17 -0500

    Duplicated FB element show/hide on media query and have working buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -34,3 +34,73 @@ function closemainMenu() {
   document.getElementById("navbar").style.position = 'fixed';
   document.getElementById("home_page_main_photo").style.marginTop = "81px";
 }
+
+document.getElementById("fb_button_1").onclick = fbslideRight;
+document.getElementById("fb_button_2").onclick = fbslideRight;
+document.getElementById("fb_button_3").onclick = fbslideLeft;
+document.getElementById("fb_button_4").onclick = fbslideLeft;
+function fbslideLeft() {
+  document.getElementById("fb_slide_1").style.left = '-1000px';
+  document.getElementById("fb_slide_2").style.left = '-500px';
+  document.getElementById("fb_slide_3").style.left = '0';
+  document.getElementById("fb_slide_4").style.left = '500px';
+  document.getElementById("fb_button_1").style.background = '#000000';
+  document.getElementById("fb_button_2").style.background = '#000000';
+  document.getElementById("fb_button_3").style.background = '#0F96DC';
+  document.getElementById("fb_button_4").style.background = '#0F96DC';
+}
+function fbslideRight() {
+  document.getElementById("fb_slide_1").style.left = '0';
+  document.getElementById("fb_slide_2").style.left = '500px';
+  document.getElementById("fb_slide_3").style.left = '1000px';
+  document.getElementById("fb_slide_4").style.left = '1500px';
+  document.getElementById("fb_button_1").style.background = '#0F96DC';
+  document.getElementById("fb_button_2").style.background = '#0F96DC';
+  document.getElementById("fb_button_3").style.background = '#000000';
+  document.getElementById("fb_button_4").style.background = '#000000';
+}
+
+document.getElementById("fb_condense_button_1").onclick = fbsetSlide1;
+document.getElementById("fb_condense_button_2").onclick = fbsetSlide2;
+document.getElementById("fb_condense_button_3").onclick = fbsetSlide3;
+document.getElementById("fb_condense_button_4").onclick = fbsetSlide4;
+function fbsetSlide1() {
+  document.getElementById("fb_slide_condense_1").style.left = '0';
+  document.getElementById("fb_slide_condense_2").style.left = '500px';
+  document.getElementById("fb_slide_condense_3").style.left = '1000px';
+  document.getElementById("fb_slide_condense_4").style.left = '1500px';
+  document.getElementById("fb_condense_button_1").style.background = '#0F96DC';
+  document.getElementById("fb_condense_button_2").style.background = '#000000';
+  document.getElementById("fb_condense_button_3").style.background = '#000000';
+  document.getElementById("fb_condense_button_4").style.background = '#000000';
+}
+function fbsetSlide2() {
+  document.getElementById("fb_slide_condense_1").style.left = '-500px';
+  document.getElementById("fb_slide_condense_2").style.left = '0';
+  document.getElementById("fb_slide_condense_3").style.left = '500px';
+  document.getElementById("fb_slide_condense_4").style.left = '1000px';
+  document.getElementById("fb_condense_button_1").style.background = '#000000';
+  document.getElementById("fb_condense_button_2").style.background = '#0F96DC';
+  document.getElementById("fb_condense_button_3").style.background = '#000000';
+  document.getElementById("fb_condense_button_4").style.background = '#000000';
+}
+function fbsetSlide3() {
+  document.getElementById("fb_slide_condense_1").style.left = '-1000px';
+  document.getElementById("fb_slide_condense_2").style.left = '-500px';
+  document.getElementById("fb_slide_condense_3").style.left = '0';
+  document.getElementById("fb_slide_condense_4").style.left = '500px';
+  document.getElementById("fb_condense_button_1").style.background = '#000000';
+  document.getElementById("fb_condense_button_2").style.background = '#000000';
+  document.getElementById("fb_condense_button_3").style.background = '#0F96DC';
+  document.getElementById("fb_condense_button_4").style.background = '#000000';
+}
+function fbsetSlide4() {
+  document.getElementById("fb_slide_condense_1").style.left = '-1500px';
+  document.getElementById("fb_slide_condense_2").style.left = '-1000px';
+  document.getElementById("fb_slide_condense_3").style.left = '-500px';
+  document.getElementById("fb_slide_condense_4").style.left = '0';
+  document.getElementById("fb_condense_button_1").style.background = '#000000';
+  document.getElementById("fb_condense_button_2").style.background = '#000000';
+  document.getElementById("fb_condense_button_3").style.background = '#000000';
+  document.getElementById("fb_condense_button_4").style.background = '#0F96DC';
+}
